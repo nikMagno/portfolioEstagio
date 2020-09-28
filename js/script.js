0dc04eaaa7041cf2423f133636341219016a660d
Nikolas Magno <nmagno.dev@gmail.com>
2020-09-28 16:49:01 -0300

    modificando menu mobile

--- js/script.js
+++ js/script.js
@@ -11,12 +11,7 @@
   /* Menu Mobile */
 
   $(function(){
-    $('nav.mobile i').click(function(){
-      var el = $(this).parent().find('ul');
-        if(el.is(':visible') == false){
-          el.fadeIn();
-        }else{
-          el.fadeOut();
-        }
+    $('.mobile').click(function(){
+      $('.mobile').find('ul').slideToggle();
     })
-  })
+  });
